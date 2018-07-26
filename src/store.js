2e5f884677935705d79b8e02b855ff69e5e4c048
Rashmi <rashmiap.10@gmail.com>
2018-07-26 14:22:25 +0530

    store created. dummy default state added synced browser history with store

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { createStore, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+//import the root reducers
+import rootReducer  from './reducers/index';
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for default data
+const defaultState = {
+  posts,
+  comments
+}
+
+const store = createStore(rootReducer, defaultState);
+
+//export const history = syncHistoryWithStore(createBrowserHistory, store);
+export const history = syncHistoryWithStore(createBrowserHistory(), store);
+
+export default store;
