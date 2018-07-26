e6e1c276168181a8b64fe5d02327da2cf7ec942f
Rashmi <rashmiap.10@gmail.com>
2018-07-26 14:32:37 +0530

    reducers created - dummy ones for now

---
+++ src/reducers/posts.js
@@ -0,0 +1,10 @@
+//a reducer takes in 2 things
+// 1. action (info about what happened)
+// 2. copy of current state
+
+function posts(state = [], action){
+  console.log(state,action);
+  return state;
+}
+
+export default posts;
