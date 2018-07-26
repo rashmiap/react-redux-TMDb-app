0384dfc571df5d223d1d96ee7647574f523f7e60
Rashmi <rashmiap.10@gmail.com>
2018-07-26 14:33:38 +0530

    Provider component linked with store. Redux setup config

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import App from './Components/App';
 // import react-router deps
 import { Router, IndexRoute} from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
 )
-
-
 ReactDOM.render(router, document.getElementById('root'));
