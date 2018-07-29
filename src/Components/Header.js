e8e9953b1cccdab081183667a51ed63466e08675
Rashmi <rashmiap.10@gmail.com>
2018-07-29 12:51:21 +0530

    Header top component added - with logo icon

---
+++ src/Components/Header.js
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { headerStyles } from '../Styles/styles';
+import logo from '../Assets/IMDB_Logo.svg';
+
+class Header extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    const { classes } = this.props;
+    return(
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Link to="/">
+              <img src={logo} className={classes.logo}/>
+            </Link>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(headerStyles)(Header);
