f1425534a72e7cda6637b942e74b01a06036b65b
Rashmi <rashmiap.10@gmail.com>
2018-07-30 14:35:18 +0530

    router changed with browserRouter mistake fixed

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import './Css/index.css';
 import App from './Components/App';
 // import react-router deps
 import { Router, IndexRoute} from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
 const router = (
   <Provider store={store}>
-    <Router history={history}>
+    <BrowserRouter history={history}>
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>
 )
 ReactDOM.render(router, document.getElementById('root'));
