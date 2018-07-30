1ef882b2cc14a44208354d6239ba42ac22bf4505
Rashmi <rashmiap.10@gmail.com>
2018-07-30 15:31:26 +0530

    card inline styles updated

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -9,3 +9,15 @@ export const headerStyles = {
     backgroundColor: '#000'
   }
 };
+
+export const cardTileStyles = {
+  card: {
+    maxWidth: 300,
+    marginRight: 20,
+    marginBottom: 20,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+}
