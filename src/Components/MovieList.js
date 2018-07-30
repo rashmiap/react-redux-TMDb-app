85ea20b7b1bb68d899def902af496cfa303cf8a2
Rashmi <rashmiap.10@gmail.com>
2018-07-30 15:32:47 +0530

    movie tile component imported maping through static movie posts

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import MovieTile from './MovieTile';
 
 export default class MovieList extends Component {
   constructor(props){
     super(props);
   }
+  __renderTiles(){
+    let renderBlocks = [];
+    renderBlocks = this.props.posts.map((key, index) => {
+      return (
+        <MovieTile key={index} />
+      );
+    })
+    return renderBlocks;
+  }
   render(){
     return(
-      <div>
+      <div className="movie-listing">
         <p>Movie listing</p>
+        <div className="movie-listing__blocks">
+          {this.__renderTiles()}
+        </div>
       </div>
     )
   }
