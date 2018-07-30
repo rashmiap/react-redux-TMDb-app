16d662be9486f16ff619d2d98adb60aae26e6df7
Rashmi <rashmiap.10@gmail.com>
2018-07-30 19:53:21 +0530

    static data change

--- src/data/posts.js
+++ src/data/posts.js
@@ -2,32 +2,38 @@ const posts = [
   {
     name: 'Sam',
     id: 1,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 1,
   },
   {
     name: 'Sam',
     id: 2,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 2,
   },
   {
     name: 'Sam',
     id: 3,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 3,
   },
   {
     name: 'Sam',
     id: 4,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 4,
   },
   {
     name: 'Sam',
     id: 5,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 5,
   },
   {
     name: 'Sam',
     id: 6,
-    email: 'somewhere@gmail.com'
+    email: 'somewhere@gmail.com',
+    code: 6,
   }
 ];
 export default posts;
