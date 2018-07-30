ef82000e803a31af1e28966fa89fae8ca2c2d617
Rashmi <rashmiap.10@gmail.com>
2018-07-30 14:36:32 +0530

    The problem is that Redux implements shouldComponentUpdate and there’s no indication that anything has changed if it isn’t receiving props from the router. This is straightforward to fix. Connect your component and wrap it in withRouter. Navigation issue of url changing but component not getting updated resolved

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
@@ -12,6 +13,6 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch){
   return bindActionCreators(actionCreators, dispatch);
 }
-const App = connect(mapStateToProps, mapDispatchToProps)(Main);
+const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
 export default App;
