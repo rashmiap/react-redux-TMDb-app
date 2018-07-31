f0970f178d4da2b2bb9038f8514ec2f651129563
Rashmi <rashmiap.10@gmail.com>
2018-07-31 11:46:26 +0530

    rename posts to movies data store

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,39 +1,45 @@
-const posts = [
+const movies = [
+  {
+    name: 'Sam',
+    id: 0,
+    email: 'somewhere@gmail.com',
+    code: 1,
+    saved: false,
+  },
   {
     name: 'Sam',
     id: 1,
     email: 'somewhere@gmail.com',
-    code: 1,
+    code: 2,
+    saved: false,
   },
   {
     name: 'Sam',
     id: 2,
     email: 'somewhere@gmail.com',
-    code: 2,
+    code: 3,
+    saved: false,
   },
   {
     name: 'Sam',
     id: 3,
     email: 'somewhere@gmail.com',
-    code: 3,
+    code: 4,
+    saved: false,
   },
   {
     name: 'Sam',
     id: 4,
     email: 'somewhere@gmail.com',
-    code: 4,
+    code: 5,
+    saved: false,
   },
   {
     name: 'Sam',
     id: 5,
     email: 'somewhere@gmail.com',
-    code: 5,
-  },
-  {
-    name: 'Sam',
-    id: 6,
-    email: 'somewhere@gmail.com',
     code: 6,
+    saved: false,
   }
 ];
-export default posts;
+export default movies;
