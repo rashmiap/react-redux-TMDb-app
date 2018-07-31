1bc7d9a3c5c9bae0ba8d3f9f8a15dcb9ba197c80
Rashmi <rashmiap.10@gmail.com>
2018-07-31 14:40:05 +0530

    Router component cleanup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './Css/index.css';
 // import components
 import App from './Components/App';
 // import react-router deps
-import { Router, IndexRoute} from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
