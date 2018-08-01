0a82e06fd864d642437759606c6db70ff6cb1e4a
Rashmi <rashmiap.10@gmail.com>
2018-08-01 16:56:13 +0530

    Route fix reusable component fixes

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MovieList from './MovieList';
-import ShowList from './ShowList';
 import MovieDetail from './MovieDetail';
 import NotFound from './NotFound';
 
 const Movie = (props) => {
   return <main className="Movie-content">
     <Switch>
-      <Route exact path='/' render={() => <MovieList {...props} movies={props.movies[0].results} />} />
-      <Route path='/movies' render={() => <MovieList {...props} movies={props.movies[0].results} />} />
-      <Route path='/shows' render={() => <ShowList {...props} shows={props.shows[0].results} />} />
-      <Route exact path='/view/:postId' render={(routeProps) => <MovieDetail {...{...props,...routeProps}} movies={props.movies[0].results} />} />
+      <Route exact path='/' render={() => <MovieList {...props} movies={props.movies[0].results} movieType={true} />} />
+      <Route path='/movies' render={() => <MovieList {...props} movies={props.movies[0].results} movieType={true} />} />
+      <Route path='/shows' render={() => <MovieList {...props} movies={props.shows[0].results} movieType={false} />} />
+      <Route exact path='/view/:postId' render={(routeProps) => <MovieDetail {...{...props,...routeProps}} movies={props.movies[0].results} shows={props.shows[0].results} />} />
       <Route component={NotFound} />
     </Switch>
   </main>
