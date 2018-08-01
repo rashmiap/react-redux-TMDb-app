329a58bd470ce9b275928467c1ca8b0282bc2230
Rashmi <rashmiap.10@gmail.com>
2018-08-01 11:03:01 +0530

    router used as alias

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import './Css/index.css';
 // import components
 import App from './Components/App';
 // import react-router deps
-import { BrowserRouter } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
 const router = (
   <Provider store={store}>
-    <BrowserRouter history={history}>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 )
 ReactDOM.render(router, document.getElementById('root'));
