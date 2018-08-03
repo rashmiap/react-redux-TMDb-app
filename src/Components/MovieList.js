8a120d980199294f4f9c1e2ceec12878fbc0afb4
Rashmi <rashmiap.10@gmail.com>
2018-08-03 10:36:45 +0530

    pinging api on component did mount dispatching fetchdata method

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import MovieTile from './MovieTile';
 
 export default class MovieList extends Component {
+  constructor(props){
+    super(props);
+  }
   componentDidMount () {
     const apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=28967d69513d49d94603253876b995a8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
-    //this.props.fetchData(apiUrl);
+    this.props.fetchData(apiUrl);
   }
 
   __renderTiles(){
