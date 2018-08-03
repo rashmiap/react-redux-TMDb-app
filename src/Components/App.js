e2b01f5f315a6249ac962e351994ea5d937b4ad9
Rashmi <rashmiap.10@gmail.com>
2018-08-03 10:42:40 +0530

    changed initial store - fetch from api

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,16 +4,29 @@ import { withRouter } from 'react-router';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
-const mapStateToProps = (state, ownProps) => {
+// const mapStateToPropsdo = (state, ownProps) => {
+//   return {
+//     movies: (state.movies),
+//     shows: (state.shows),
+//     ownProps
+//   }
+// }
+//
+// function mapDispatchToPropsdo(dispatch){
+//   return bindActionCreators(actionCreators, dispatch);
+// }
+const mapStateToProps = (state) => {
   return {
-    movies: (state.movies),
-    shows: (state.shows),
-    ownProps
-  }
-}
-function mapDispatchToProps(dispatch){
-  return bindActionCreators(actionCreators, dispatch);
-}
+        responseItem: state.fetchSuccess,
+        hasErrored: state.fetchErrored,
+        isLoading: state.fetchIsLoading,
+        movies: state.movies,
+        shows: state.shows,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actionCreators, dispatch);
+};
 const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
 export default App;
