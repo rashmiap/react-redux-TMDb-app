a177bb65483ee3d84fafd8e3c1726febac4b60f7
Rashmi <rashmiap.10@gmail.com>
2018-08-03 13:02:31 +0530

    store -cleanup

--- src/store.js
+++ src/store.js
@@ -1,11 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createBrowserHistory } from 'history';
-import { syncHistoryWithStore } from 'react-router-redux';
 import thunk from 'redux-thunk';
-//import the root reducers
 import rootReducer  from './reducers/index';
-import movies from './data/movies';
-import shows from './data/shows';
 
 /**
 * The createStore method takes 3 arguments:
@@ -15,17 +10,10 @@ const enhancers = compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
-//create an object for default data
-
-const store = createStore(rootReducer, enhancers);
-export const history = syncHistoryWithStore(createBrowserHistory(), store);
-
-//export default store;
-
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        enhancers
     );
 }
