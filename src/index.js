6497553ad3b09352a58c645d2fe34c9b39739275
Rashmi <rashmiap.10@gmail.com>
2018-08-04 17:21:02 +0530

    removed default store - configstore

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import CSS
 import './Css/index.css';
-// import components
 import App from './Components/App';
-// import react-router deps
 import { BrowserRouter, Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import store, { history } from './store';
 import configureStore from './store';
-import movies from './data/movies';
-import shows from './data/shows';
 
-const defaultState = {
-  movies,
-  shows
-}
-const store = configureStore(defaultState);
-
+const store = configureStore();
 const router = (
   <Provider store={store}>
     <BrowserRouter>
