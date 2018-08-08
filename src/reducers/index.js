befa0f8f34df4f9af4ed1611fd3fb92f5157cfa8
Rashmi <rashmiap.10@gmail.com>
2018-08-08 21:34:13 +0530

    root reducer combined

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,22 @@
 //creating root reducers
-import { combineReducers } from 'redux';
+import { combineReducers, reduceReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { fetchSuccess, fetchIsLoading, fetchErrored, movies, fetchDetailsSuccess,
-fetchCastSuccess, fetchCastIsLoading,  fetchCastDataErrored } from './movies';
+import { responseItem } from './responseItem';
+import { isLoading } from './isLoading';
+import { hasErrored } from './hasErrored';
+import { fullDetails } from './fullDetails';
+import { castDetails } from './castDetails';
+import { isCastLoading } from './isCastLoading';
+import { hasCastErrored } from './hasCastErrored';
 
 const rootReducer = combineReducers({
-  movies,
-  fetchErrored,
-  fetchIsLoading,
-  fetchSuccess,
-  fetchDetailsSuccess,
-  fetchCastSuccess,
-  fetchCastIsLoading,
-  fetchCastDataErrored,
+  isLoading,
+  responseItem,
+  hasErrored,
+  fullDetails,
+  castDetails,
+  isCastLoading,
+  hasCastErrored,
   routing : routerReducer });
 
 export default rootReducer;
