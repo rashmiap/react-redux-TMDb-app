370c9e9ced64e4915a1e0e8b9b544db844e0a677
Rashmi <rashmiap.10@gmail.com>
2018-08-10 11:20:39 +0530

    updated view

--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -9,7 +9,7 @@ export default class MovieDetail extends Component {
   }
   componentDidMount(){
     const movieId = this.props.match.params.postId;
-    this.props.fetchDetailsData(`https://api.themoviedb.org/3/movie/${movieId}?api_key=28967d69513d49d94603253876b995a8&language=en-US`);
+    this.props.fetchCurrentMovie(movieId, this.props.movies);
     this.props.fetchCastData(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=28967d69513d49d94603253876b995a8`);
   }
   __renderCast(){
@@ -17,7 +17,7 @@ export default class MovieDetail extends Component {
     renderCastBlock = this.props.castDetails.cast !== undefined ?
      this.props.castDetails.cast.map((item, index) => {
        return (
-        <div key={item.id} className="cast-single">
+        <div key={item.credit_id} className="cast-single">
           <div className="cast-single__header"
           style={Object.assign({ backgroundImage:`url(https://image.tmdb.org/t/p/original${item.profile_path})`},cardDetails.castHeader)}>
             &nbsp;
@@ -33,39 +33,30 @@ export default class MovieDetail extends Component {
     return renderCastBlock;
   }
   render(){
-    const postId = this.props.match.params.postId;
-    const { title, overview, release_date, vote_average, backdrop_path, poster_path } = this.props.fullDetails;
-
-    if (this.props.hasErrored) {
-      return <div className="movie-listing__error">
-          <h3> Oops! There was an error loading the items </h3>
-      </div>
-    }
-
-    if (this.props.isLoading) {
-      return <div className="movie-listing__error">
-            <CircularProgress size={150} thickness={2}/>
-      </div>;
-    }
-
     return(
       <div className="details">
-        <div className="details-header" style={Object.assign({ backgroundImage:`url(https://image.tmdb.org/t/p/original${backdrop_path})`},cardDetails.headerStyles)}>
-          &nbsp;
-        </div>
-        <div className="details-body">
-          <div className="details-content">
-            <h2>{title}</h2>
-            <div>
-              <p><strong>Overview :</strong> {overview}</p>
+        {
+          this.props.currentMovie.length > 0 ?
+          <div>
+            <div className="details-header" style={Object.assign({ backgroundImage:`url(https://image.tmdb.org/t/p/original${this.props.currentMovie[0].backdrop_path})`},cardDetails.headerStyles)}>
+              &nbsp;
             </div>
-            <p>Release date: {release_date}</p>
+            <div className="details-body">
+              <div className="details-content">
+                <h2>{this.props.currentMovie[0].title}</h2>
+                <div>
+                  <p><strong>Overview :</strong> {this.props.currentMovie[0].overview}</p>
+                </div>
+                <p>Release date: {this.props.currentMovie[0].release_date}</p>
+              </div>
+            </div>
+            <div className="details-cast">
+              <p style={cardDetails.castText}>Full Cast</p>
+              {this.__renderCast()}
+            </div>
           </div>
-          <div className="details-cast">
-            <p style={cardDetails.castText}>Full Cast</p>
-            {this.__renderCast()}
-          </div>
-        </div>
+          : <p>wait for it</p>
+        }
       </div>
     )
   }
