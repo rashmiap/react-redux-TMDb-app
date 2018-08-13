c6231a2b6a0ee176f37ac8d60888ff4c35481adf
Rashmi <rashmiap.10@gmail.com>
2018-08-13 15:40:15 +0530

    mobile responsive view with static navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -9,16 +9,16 @@ const Navigation = (props) => {
   return(
     <nav className="Nav">
       <NavLink activeClassName="Nav-active" exact to="/">
-        <img src={movieNav} width={35} style={{marginRight: '10px'}} alt='movie logo'/>
-        Latest Movies
+        <img src={movieNav} width={35} alt='movie logo'/>
+        <p className="Nav-text Nav-movies">Latest Movies</p>
       </NavLink>
       <NavLink activeClassName="Nav-active" to="/shows">
-        <img src={showNav} width={35} style={{marginRight: '20px'}} alt='shows logo'/>
-        TV Shows
+        <img src={showNav} width={35} alt='shows logo'/>
+        <p className="Nav-text Nav-shows">TV Shows</p>
       </NavLink>
       <NavLink activeClassName="Nav-active" to="/saved">
-        <img src={savedNav} width={35} style={{marginRight: '8px'}} alt='saved logo'/>
-        Saved Cards
+        <img src={savedNav} width={35} alt='saved logo'/>
+        <p className="Nav-text Nav-saved">Saved Cards</p>
       </NavLink>
     </nav>
   )
