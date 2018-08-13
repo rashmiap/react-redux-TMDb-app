72cae96f0808a5807faecbb0608ddf3ea5ab1df3
Rashmi <rashmiap.10@gmail.com>
2018-08-13 09:50:54 +0530

    current movie reducer updated can fetch current movie and update toggle save unsave

--- src/reducers/currentMovie.js
+++ src/reducers/currentMovie.js
@@ -1,10 +1,21 @@
-import { FETCH_CURRENT_MOVIE } from '../constants/ActionTypes'
+import { FETCH_CURRENT_MOVIE, TOGGLE_SAVE } from '../constants/ActionTypes'
 
 export function currentMovie(state = [], action){
   switch (action.type) {
     case FETCH_CURRENT_MOVIE:
       return action.movies.filter(item => item.id == action.movieId);
-  default:
-    return state;
+
+    case TOGGLE_SAVE:
+      const newState = [...state] // clone the array
+      if(newState.length > 0){
+        newState[0].saved = !newState[0].saved
+        return newState;
+      }
+      else{
+        return state;
+      }
+
+    default:
+      return state;
   }
 }
