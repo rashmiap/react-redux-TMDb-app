37696856a33e1f9bc82d053eb4926aa47becb8bd
rashmiap <rashmiap.10@gmail.com>
2018-11-27 15:54:22 +0530

    added redux-thunk package fixes issue filter map using wrong index

--- src/Components/SavedTile.js
+++ src/Components/SavedTile.js
@@ -9,38 +9,44 @@ export default class SavedTile extends Component {
     window.scrollTo(0,0);
   }
   __renderMovies(){
-    let renderBlock = [];
-    this.props.movies.results !== undefined ?
-     this.props.movies.results.filter((key, index) => {
-       if(key.saved){
-         renderBlock.push(<MovieTile key={index} {...this.props} movies={this.props.movies.results} i={index} movieType={'movies'}/>)
-       }
-       return renderBlock;
-     })
-     : [];
-    return renderBlock.length > 0 ? renderBlock : <p className="no-saved">Looks like there no saved movies</p>;
+   let renderBlock = [];
+   renderBlock = this.props.movies.results !== undefined ?
+    this.props.movies.results
+    .map((item, key) => {
+      if (item.saved === true) {
+        return(
+          <MovieTile key={key} {...this.props} movies={this.props.movies.results} i={key} movieType={'movies'}/>
+        )
+      }
+      return [];
+    })
+    : <p className="no-saved">Looks like there no saved movies</p>;
+   return renderBlock.length > 0 ? renderBlock : <p className="no-saved">Looks like there no saved movies</p>;
   }
   __renderShows(){
-    let renderShows = [];
-    this.props.shows.results !== undefined ?
-    this.props.shows.results.filter((key, index) => {
-      if(key.saved){
-        renderShows.push(<MovieTile key={index} {...this.props} movies={this.props.shows.results} i={index} movieType={'shows'}/>)
-      }
-      return renderShows;
-    }): [];
-    return renderShows.length > 0 ? renderShows : <p className="no-saved">Looks like there no saved shows</p>;
+   let renderShows = [];
+   renderShows = this.props.shows.results !== undefined ?
+   this.props.shows.results.map((item, key) => {
+     if(item.saved === true){
+       return(
+         <MovieTile key={key} {...this.props} movies={this.props.shows.results} i={key} movieType={'shows'}/>
+       )
+     }
+     return [];
+   }): <p className="no-saved">Looks like there no saved shows</p>;
+   return renderShows.length > 0 ? renderShows : <p className="no-saved">Looks like there no saved shows</p>;
   }
+
   render(){
     return(
       <div className="movie-listing">
         <h3>Saved Movies</h3>
         <div className="movie-listing__blocks">
-            { this.__renderMovies() }
+            {this.__renderMovies()}
         </div>
         <h3 className="movie-listing__header">Saved Shows</h3>
         <div className="movie-listing__blocks">
-            { this.__renderShows() }
+            {this.__renderShows()}
         </div>
       </div>
     )
